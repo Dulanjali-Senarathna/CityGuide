efbebf5c17b4f50e573a1994bf41ec5ef497646f
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-08-14 17:11:08 +0530

    Onboarding Screen Shared Preferences Done

--- app/src/main/java/com/example/cityguide/Common/SplashScreen.java
+++ app/src/main/java/com/example/cityguide/Common/SplashScreen.java
@@ -3,6 +3,7 @@ package com.example.cityguide.Common;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.WindowManager;
@@ -24,6 +25,8 @@ public class SplashScreen extends AppCompatActivity {
 
     //Animations
     Animation sideAnim,bottomAnim;
+
+    SharedPreferences onBoardingScreen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +50,26 @@ public class SplashScreen extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent = new Intent(getApplicationContext(), OnBoarding.class);
-                startActivity(intent);
-                finish();
+                onBoardingScreen = getSharedPreferences("onBoardingScreen",MODE_PRIVATE);
+                boolean isFirstTime = onBoardingScreen.getBoolean("firstTime",true);
+
+                if(isFirstTime){
+
+                    SharedPreferences.Editor editor = onBoardingScreen.edit();
+                    editor.putBoolean("firstTime",false);
+                    editor.commit();
+
+                    Intent intent = new Intent(getApplicationContext(), OnBoarding.class);
+                    startActivity(intent);
+                    finish();
+                }
+                else {
+                    Intent intent = new Intent(getApplicationContext(), UserDashboard.class);
+                    startActivity(intent);
+                    finish();
+                }
+
+
 
             }
         },SPLASH_TIMER);
