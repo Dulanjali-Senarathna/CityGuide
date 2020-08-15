59b33d0abc9761c32577826826d0d718522b7803
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-08-15 12:39:01 +0530

    Recyclerview and Cardview design Done

--- app/src/main/java/com/example/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/example/cityguide/User/UserDashboard.java
@@ -1,18 +1,89 @@
 package com.example.cityguide.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.view.WindowManager;
 
+import com.example.cityguide.HelperClasses.HomeAdapter.CategoriesAdapter;
+import com.example.cityguide.HelperClasses.HomeAdapter.CategoriesHelperClass;
+import com.example.cityguide.HelperClasses.HomeAdapter.FeaturedAdapter;
+import com.example.cityguide.HelperClasses.HomeAdapter.FeaturedHelperClass;
+import com.example.cityguide.HelperClasses.HomeAdapter.MostViewedAdapter;
+import com.example.cityguide.HelperClasses.HomeAdapter.MostViewedHelperClass;
 import com.example.cityguide.R;
 
+import java.util.ArrayList;
+
 public class UserDashboard extends AppCompatActivity {
+
+    RecyclerView featuredRecycler,mostViewedRecycler,categoriesRecycler;
+    RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        //Hooks
+        featuredRecycler = findViewById(R.id.featured_recycler);
+        mostViewedRecycler = findViewById(R.id.mv_recycler);
+        categoriesRecycler = findViewById(R.id.ct_recycler);
+
+
+        featuredRecycler();
+        mostViewedRecycler();
+        categoriesRecycler();
+    }
+
+    private void categoriesRecycler() {
+        categoriesRecycler.setHasFixedSize(true);
+       categoriesRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<CategoriesHelperClass> categoriesLocations = new ArrayList<>();
+
+        categoriesLocations.add(new CategoriesHelperClass(R.drawable.restaurant_image,"Mcdonald's"));
+        categoriesLocations.add(new CategoriesHelperClass(R.drawable.restaurant_image,"Ratnapura"));
+        categoriesLocations.add(new CategoriesHelperClass(R.drawable.restaurant_image,"Galle"));
+
+        adapter = new CategoriesAdapter(categoriesLocations);
+        categoriesRecycler.setAdapter(adapter);
+    }
+
+    private void mostViewedRecycler() {
+
+        mostViewedRecycler.setHasFixedSize(true);
+        mostViewedRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<MostViewedHelperClass> mostViewedLocations = new ArrayList<>();
+
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.mcdonalds,"Mcdonald's","hdd hdhhd hdhd "));
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.city,"Ratnapura","hdd hdhhd hdhd dhhd hhhd "));
+        mostViewedLocations.add(new MostViewedHelperClass(R.drawable.city2,"Galle","hdd hdhhd hdhd dhhd hhhd "));
+
+        adapter = new MostViewedAdapter(mostViewedLocations);
+        mostViewedRecycler.setAdapter(adapter);
+
+
+    }
+
+    private void featuredRecycler() {
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.mcdonalds,"Mcdonald's","hdd hdhhd hdhd dhhd hhhd "));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.city,"Ratnapura","hdd hdhhd hdhd dhhd hhhd "));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.city2,"Galle","hdd hdhhd hdhd dhhd hhhd "));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+
+        GradientDrawable gradient1 = new GradientDrawable(GradientDrawable.Orientation.LEFT_RIGHT,new int[]{0xffeff400,0xffaff600});
     }
 }
