8a48c993eeb2376927edd7a0864c0bfdabf06660
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-08-17 23:04:51 +0530

    Complete Form Validation

--- app/src/main/java/com/example/cityguide/Common/LoginSignup/SignUp.java
+++ app/src/main/java/com/example/cityguide/Common/LoginSignup/SignUp.java
@@ -12,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.cityguide.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUp extends AppCompatActivity {
 
@@ -20,20 +21,35 @@ public class SignUp extends AppCompatActivity {
     Button next,login;
     TextView titleText;
 
+    //Get Data variables
+    TextInputLayout fullName,username,email,password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer_sign_up);
 
-        //Hooks
+        //Hooks for animation
         backBtn = findViewById(R.id.signup_back_button);
         next = findViewById(R.id.signup_next_button);
         login = findViewById(R.id.signup_login_button);
         titleText = findViewById(R.id.signup_title_text);
 
+        //Hooks for getting data
+        fullName = findViewById(R.id.signup_fullname);
+        username = findViewById(R.id.signup_username);
+        email = findViewById(R.id.signup_email);
+        password = findViewById(R.id.signup_password);
+
+
     }
 
     public void callNextSignupScreen(View view){
+
+        if(!validateUserName() | !validateFullName() | !validateEmail() | !validatePassword()){
+            return;
+        }
+
         Intent intent = new Intent(getApplicationContext(),SignUp2ndClass.class);
 
         //Add transition
@@ -49,6 +65,85 @@ public class SignUp extends AppCompatActivity {
             startActivity(intent,options.toBundle());
         }else {
             startActivity(intent);
+        }
+    }
+
+    //Validation Functions
+    private boolean validateFullName(){
+        String val = fullName.getEditText().getText().toString().trim();
+
+        if(val.isEmpty()){
+            fullName.setError("Field can not empty");
+            return false;
+        }
+        else {
+            fullName.setError(null);
+            fullName.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateUserName(){
+        String val = username.getEditText().getText().toString().trim();
+        String checkspaces = "\\A\\w{1,20}\\z";
+
+        if(val.isEmpty()){
+            username.setError("Field can not empty");
+            return false;
+        }else if(val.length()>20){
+            username.setError("Username is too large");
+            return false;
+        }else if(!val.matches(checkspaces)){
+            username.setError("No white spaces are allowed!");
+            return false;
+        }
+
+        else {
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validateEmail(){
+        String val = email.getEditText().getText().toString().trim();
+        String checkEmail = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+        if(val.isEmpty()){
+            email.setError("Field can not empty");
+            return false;
+        }else if(!val.matches(checkEmail)){
+            email.setError("No white spaces are allowed!");
+            return false;
+        }
+
+        else {
+            email.setError(null);
+            email.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private boolean validatePassword(){
+        String val = password.getEditText().getText().toString().trim();
+        String checkPassword = "^" + "(?=.*[a-zA-Z])" +
+                "(?=.*[@#$%^&+=])" +
+                "(?=\\S+$)" +
+                ".{4,}" +
+                "$";
+
+        if(val.isEmpty()){
+            password.setError("Field can not empty");
+            return false;
+        }else if(!val.matches(checkPassword)){
+            password.setError("Password should contain four characters!");
+            return false;
+        }
+
+        else {
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
         }
     }
 }
--- app/src/main/java/com/example/cityguide/Common/LoginSignup/SignUp2ndClass.java
+++ app/src/main/java/com/example/cityguide/Common/LoginSignup/SignUp2ndClass.java
@@ -8,10 +8,16 @@ import android.os.Bundle;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.cityguide.R;
+
+import java.util.Calendar;
 
 public class SignUp2ndClass extends AppCompatActivity {
 
@@ -19,6 +25,10 @@ public class SignUp2ndClass extends AppCompatActivity {
     ImageView backBtn;
     Button next,login;
     TextView titleText;
+    RadioGroup radioGroup;
+    RadioButton selectedAge;
+    RadioButton selectedGender;
+    DatePicker datePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +40,31 @@ public class SignUp2ndClass extends AppCompatActivity {
         next = findViewById(R.id.signup_next_button);
         login = findViewById(R.id.signup_login_button);
         titleText = findViewById(R.id.signup_title_text);
+        radioGroup = findViewById(R.id.radio_group);
+        datePicker = findViewById(R.id.age_picker);
 
     }
 
-    public void callNextSignupScreen2 (View view){
+    public void call3rdSignupScreen (View view){
+
+        if(!validateGender() | !validateAge()){
+            return;
+        }
+
+        selectedGender = findViewById(radioGroup.getCheckedRadioButtonId());
+        String _gender = selectedGender.getText().toString();
+
+        int day = datePicker.getDayOfMonth();
+        int month = datePicker.getMonth();
+        int year = datePicker.getYear();
+
+        String _date = day+"/"+month+"/"+year;
+
+        String name = getIntent().getStringExtra("fullName");
+        String email = getIntent().getStringExtra("email");
+        String username = getIntent().getStringExtra("username");
+        String password = getIntent().getStringExtra("password");
+
         Intent intent = new Intent(getApplicationContext(),SignUp3rdClass.class);
 
         //Add transition
@@ -50,5 +81,27 @@ public class SignUp2ndClass extends AppCompatActivity {
         }else {
             startActivity(intent);
         }
+    }
+
+    public boolean validateGender(){
+        if(radioGroup.getCheckedRadioButtonId() == -1 ){
+            Toast.makeText(this, "Please select gender", Toast.LENGTH_SHORT).show();
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+    public boolean validateAge(){
+       int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+       int userAge = datePicker.getYear();
+       int isAgeValid = currentYear - userAge;
+
+       if(isAgeValid < 14){
+           Toast.makeText(this, "You are not eligible to apply", Toast.LENGTH_SHORT).show();
+           return false;
+       }else {
+           return true;
+       }
     }
 }
