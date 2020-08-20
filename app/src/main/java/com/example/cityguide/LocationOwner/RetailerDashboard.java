3bb90bf64554610656bbdbcc041ab57c777c0b51
Dulanjali-Senarathna <59603716+KandabadaSenarathna@users.noreply.github.com>
2020-08-20 15:12:55 +0530

    Bottom Navigation Drawer with fragments Done

--- app/src/main/java/com/example/cityguide/LocationOwner/RetailerDashboard.java
+++ app/src/main/java/com/example/cityguide/LocationOwner/RetailerDashboard.java
@@ -1,30 +1,53 @@
 package com.example.cityguide.LocationOwner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.cityguide.Databases.SessionManager;
 import com.example.cityguide.R;
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 import java.util.HashMap;
 
 public class RetailerDashboard extends AppCompatActivity {
+
+    ChipNavigationBar chipNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_retailer_dashboard);
 
-        TextView textView = findViewById(R.id.textView);
+        chipNavigationBar = findViewById(R.id.bottom_nav_menu);
+        chipNavigationBar.setItemSelected(R.id.bottom_nav_dashboard,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new RetailerDashboardFragment()).commit();
+        bottomMenu();
 
-        SessionManager sessionManager = new SessionManager(RetailerDashboard.this,SessionManager.SESSION_USERSESSION);
-       HashMap<String,String> userDetails = sessionManager.getUserDetailFromSession();
+    }
 
-       String fullName = userDetails.get(SessionManager.KEY_FULLNAME);
-       String phoneNumber = userDetails.get(SessionManager.KEY_PHONENUMBER);
+    private void bottomMenu() {
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i){
+                    case R.id.bottom_nav_dashboard:
+                        fragment = new RetailerDashboardFragment();
+                        break;
 
-       textView.setText(fullName + "\n" +phoneNumber);
+                    case R.id.bottom_nav_manage:
+                        fragment = new RetailerManageFragment();
+                        break;
+
+                    case R.id.bottom_nav_profile:
+                        fragment = new RetailerProfileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            }
+        });
     }
 }
